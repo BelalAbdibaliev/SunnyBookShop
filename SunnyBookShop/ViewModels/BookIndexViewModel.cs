eee5fcea31a84b9e9585cf27ab29f6013484e373
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-02-14 19:24:32 +0600

    Added new ViewModels

---
+++ SunnyBookShop/ViewModels/BookIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using SunnyBookShop.Models;
+
+namespace SunnyBookShop.ViewModels;
+
+public class BookIndexViewModel
+{
+    public IEnumerable<Book> Books { get; }
+    public PageViewModel PageViewModel { get; }
+    public SortViewModel SortViewModel { get; }
+
+    public BookIndexViewModel(IEnumerable<Book> books, PageViewModel pageViewModel, SortViewModel sortViewModel)
+    {
+        Books = books;
+        PageViewModel = pageViewModel;
+        SortViewModel = sortViewModel;
+    }
+}
---
+++ SunnyBookShop/ViewModels/SortViewModel.cs
@@ -0,0 +1,19 @@
+﻿using SunnyBookShop.Utils;
+
+namespace SunnyBookShop.ViewModels;
+
+public class SortViewModel
+{
+    public SortState NameSort { get; }
+    public SortState AuthorSort { get; }
+    public SortState PriceSort { get; }
+    public SortState Current { get; }
+
+    public SortViewModel(SortState sortOrder)
+    {
+        NameSort = sortOrder == SortState.NameAsc ? SortState.NameDesc : SortState.NameAsc;
+        AuthorSort = sortOrder == SortState.AuthorAsc ? SortState.AuthorDesc : SortState.AuthorAsc;
+        PriceSort = sortOrder == SortState.PriceAsc ? SortState.PriceDesc : SortState.PriceAsc;
+        Current = sortOrder;
+    }
+}
