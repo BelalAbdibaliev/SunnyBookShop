b92df87aeeac4dc68f3fa06a50eef86162cf28a9
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-02-22 17:44:58 +0600

    Little change in Book.cs model class

--- SunnyBookShop/Models/Book.cs
+++ SunnyBookShop/Models/Book.cs
@@ -7,8 +7,8 @@ namespace SunnyBookShop.Models;
 public class Book
 {
     public int Id { get; set; }
-    [Required
-    ] public string Title { get; set; } = null!;
+    [Required] 
+    public string Title { get; set; } = null!;
     [Required]
     public string Author { get; set; } = null!;
     public string? Publisher { get; set; }
