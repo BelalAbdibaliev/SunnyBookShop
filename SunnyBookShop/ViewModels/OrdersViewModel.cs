e736fbf706b9ac16f3d32a16519f814fd3d97c48
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-02-22 18:59:34 +0600

    Added OrdersViewModel

---
+++ SunnyBookShop/ViewModels/OrdersViewModel.cs
@@ -0,0 +1,9 @@
+﻿using SunnyBookShop.Models;
+
+namespace SunnyBookShop.ViewModels;
+
+public class OrdersViewModel
+{
+    public List<Order>[] Orders { get; set; } = null!;
+    public List<Order>? OrdersToUpdate { get; set; }
+}
